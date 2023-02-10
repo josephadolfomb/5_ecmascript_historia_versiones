a9bf17cbadb927064de61ec7b14cf000a3ebd1af
josephadolfomb <josephmobu@gmail.com>
2023-02-09 20:46:01 -0500

    Playground: Combina objetos JSON con el Spread Operator

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -30,3 +30,54 @@ function sum(num, ...values) {
 }
 
 sum(1, 1, 2, 3);
+
+// Ejercicio:
+// En este deafío recibirás dos objetos JSON por parámetros.
+
+// Usando el spread operator, deberás combinar ambos objetos en uno solo. Es posible que no se te envíe alguno de los dos objetos o ninguno, por lo que deberás usar estos por defecto, en su respectivo orden:
+
+// JSON 1:
+
+// {
+//     name: "Mr. Michi",
+//     food: "Pescado"
+// }
+
+// JSON 2:
+
+// {
+//     age: 12,
+//     color: "Blanco"
+// }
+
+// La solución deberá tener un input y output como la siguiente, recuerda que se te pueden o no envíar los parámetros.
+
+// Input:
+
+// solution({
+//     name: "Bigotes",
+//     food: "Pollito"
+// }),
+
+// Output:
+
+// {
+//     name: "Bigotes",
+//     food: "Pollito",
+//     age: 12,
+//     color: "Blanco"
+// }
+
+function solution(json1 = {
+    name: "Mr. Michi",
+    food: "Pescado"
+}, json2 = {
+    age: 12,
+    color: "Blanco"
+}) {
+    let output = { ...json1, ...json2 };
+    console.log(output);
+}
+
+solution({"name":"Atena","food":"Leche"}, {"age":2,"color":"Café"});
+

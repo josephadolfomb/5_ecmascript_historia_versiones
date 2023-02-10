ce84964c6e5031ebbd338da9773bb7c08a99cbde
josephadolfomb <josephmobu@gmail.com>
2023-02-10 18:08:21 -0500

    Playground: Obtén una lista de películas 14/35

---
+++ src/es6/exercise.js
@@ -0,0 +1,29 @@
+import { getData } from "./api.js";
+
+export function solution() {
+  return getData().then(movies => movies);
+}
+
+console.log(solution());
+
+// En este desafío tendrás un archivo llamado api.js que estará al mismo nivel que tu archivo exercise.js.
+
+// Este archivo tendrá una función asíncrona getData() que te retornará una lista de resultados misteriosa.
+
+// Tu función solution() deberá retornar la misma lista de resultados que te brinda la función getData().
+
+// Input:
+
+// solution();
+
+// Ouput:
+
+// [{
+//     movie: "El despertar de los michis",
+//     year: 2021,
+//     protagonist: "Mr. Michi"
+// }, {
+//     movie: "101 Michis",
+//     year: 2019,
+//     protagonist: "Tommy Michiguire"
+// }]

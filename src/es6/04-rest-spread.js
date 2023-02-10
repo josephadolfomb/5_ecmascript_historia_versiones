1a1c08bee099e571e98eedbdfd1e90205cdc1d2b
josephadolfomb <josephmobu@gmail.com>
2023-02-09 20:22:56 -0500

    ES6: spread operator

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -11,3 +11,22 @@ console.log(a, fruits[1]);
 let user = {username: 'Oscar', age: 34};
 let {username, age} = user;
 console.log(username, user.age);
+
+// spread operator
+
+let person = {name:'Oscar', age: 28 };
+let country = 'MX';
+
+let data = {id:1, ...person, country};
+console.log(data);
+
+//rest parameter
+// dan ventaja para usar arreglos objetos etc
+
+function sum(num, ...values) {
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+}
+
+sum(1, 1, 2, 3);

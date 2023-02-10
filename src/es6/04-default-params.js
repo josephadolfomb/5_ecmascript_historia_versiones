bd6a9341b4574ea8f0736983bcde3e93aefafa84
josephadolfomb <josephmobu@gmail.com>
2023-02-09 20:02:26 -0500

    ES6: parámetros por defecto

---
+++ src/es6/04-default-params.js
@@ -0,0 +1,19 @@
+//antes y despues
+
+function newUser(name, age, country){
+    var name = name || 'Oscar';
+    var age = age || 34;
+    var country = country || 'MX';
+    console.log(name, age, country);
+}
+
+newUser();
+newUser('David', 15, 'CO');
+
+//nueva forma
+function newAdmin(name = 'Oscar', age = 32, country = 'CL') {
+    console.log(name, age, country);
+}
+
+newAdmin();
+newAdmin('Ana', 28, 'PE');

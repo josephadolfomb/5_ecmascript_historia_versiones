ec1d5624d8b0ea23a960995f37738682b8d2d3e1
josephadolfomb <josephmobu@gmail.com>
2023-02-21 21:23:34 -0500

    ES11: BigInt y Nullish 27/35

---
+++ src/es11/01-bigint.js
@@ -0,0 +1,6 @@
+const aBigNumber = 743473473843284289420349343n;
+const anotherBigNumber = BigInt(743473473843284289420349343);
+const anotherBigNumber2 = BigInt(743473473843284289420349343n);
+console.log(aBigNumber);
+console.log(anotherBigNumber);
+console.log(anotherBigNumber2);
